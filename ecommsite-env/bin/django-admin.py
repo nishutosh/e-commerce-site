ce953eb8e3ebdae7e09df6f005e4824e0edc2451
nishutosh <nishutosh002@gmail.com>
2017-12-19 09:25:42 +0530

    vir-env removed

--- ecommsite-env/bin/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!/media/ankit/work/projects/e-commerce-site-front-end/e-commerce-site/ecommsite-env/bin/python3
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
